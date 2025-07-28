bab809e8023e3e18e6facefd9c8559d91c679f23
Esubalew Amenu <54699823+EsubalewAmenu@users.noreply.github.com>
2025-07-28 16:35:27 +0300

    apps restructured

--- apps/chicks/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChicksConfig(AppConfig):
-    name = 'chicks'
--- apps/hatchery/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HatcheryConfig(AppConfig):
-    name = 'hatchery'
